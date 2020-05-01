8d6cc677b560def3c2ea1a344d515a654b3a430b
Shio <61821471+Shio7@users.noreply.github.com>
2020-05-01 10:08:03 +0900

    테스트토큰
    개발후삭제될토큰

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands 
 
 token = "paste token here" #토큰 붙여넣으세요
+#테스트 토큰: NzA1NTg1Nzk2NjY4NzE5MTY0.Xqt2EQ.DHOh7vGCU6nOLitoDrm-yPY91M8
 
 class Bot(commands.Bot):
     def __init__(self):
